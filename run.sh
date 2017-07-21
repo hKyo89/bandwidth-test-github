209a6352f1c6957aa980275231da240e91a4bc17
Argi Karunia <hkyo89@yahoo.co.jp>
2017-07-21 08:00:13 +0000

    new package

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 BASEDIR=$(dirname "$0")
 
-outer=500
-inner=100
+outer=5000
+inner=2
 PREFIX="Creating package"
 PKGDIR=$BASEDIR/pkg
 
@@ -12,17 +12,26 @@ echo -ne "$PREFIX 0%\033[0K\r"
 for (( i = 1; i <= $outer; i++ )); do
   if [[ $i = 1 ]]; then 
     rm -rf $PKGDIR
-    mkdir -p $PKGDIR/$i
 
     for (( j = 1; j <= $inner; j++ )); do
-      cp $BASEDIR/testfile $PKGDIR/$i/testfile-$j
+      for (( k = 1; k <= $inner; k++ )); do
+        for (( a = 1; a <= $inner; a++ )); do
+          for (( b = 1; b <= $inner; b++ )); do
+            mkdir -p $PKGDIR/$i/$j/$k/$a/$b
+            for (( c = 1; c <= $inner; c++ )); do
+              cp $BASEDIR/testfile $PKGDIR/$i/$j/$k/$a/$b/testfile-$c
+            done
+          done
+        done
+      done
     done
   else
     mkdir -p $PKGDIR/$i
     cp -rf $PKGDIR/1/* $PKGDIR/$i/ 
   fi
-        
-  n=`echo "$i * 100 / $outer" | bc`
+  
+  n=`echo $i $outer | awk '{div=$1/$2; printf"%0.2f\n", div }'`
+  # n=`echo "$i * 100 / $outer" | bc`
   echo -ne "$PREFIX $n%\033[0K\r"
 done
 
