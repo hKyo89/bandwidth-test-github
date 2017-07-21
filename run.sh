50deb3bf715c7dcfb86a2dae30c4e96f3d6f0f2d
hkyo89 <hkyo89@yahoo.co.jp>
2017-07-21 14:16:49 +0700

    add more packages

--- run.sh
+++ run.sh
@@ -2,24 +2,24 @@
 
 BASEDIR=$(dirname "$0")
 
-outer=100
-inner=150
+outer=500
+inner=100
 PREFIX="Creating package"
+PKGDIR=$BASEDIR/pkg
 
 echo -ne "$PREFIX 0%\033[0K\r"
 
 for (( i = 1; i <= $outer; i++ )); do
-  PKGDIR=$BASEDIR/pkg
-
   if [[ $i = 1 ]]; then 
-    rm -rf $PKGDIR/$i
+    rm -rf $PKGDIR
     mkdir -p $PKGDIR/$i
 
     for (( j = 1; j <= $inner; j++ )); do
       cp $BASEDIR/testfile $PKGDIR/$i/testfile-$j
     done
   else
-    cp -rf $PKGDIR/1 $PKGDIR/$i 
+    mkdir -p $PKGDIR/$i
+    cp -rf $PKGDIR/1/* $PKGDIR/$i/ 
   fi
         
   n=`echo "$i * 100 / $outer" | bc`
