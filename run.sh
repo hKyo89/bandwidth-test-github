e062c70d7a821065b46c441cf59d2b1b5b83a46c
Argi Karunia <hkyo89@yahoo.co.jp>
2017-07-21 10:39:46 +0000

    new packages

--- run.sh
+++ run.sh
@@ -12,12 +12,25 @@ echo -ne "$PREFIX 0%\033[0K\r"
 for (( i = 1; i <= $outer; i++ )); do
   if [[ $i = 1 ]]; then 
     rm -rf $PKGDIR
+    mkdir -p $PKGDIR/$i
+    cp $BASEDIR/testfile $PKGDIR/testfile-$i
 
     for (( j = 1; j <= $inner; j++ )); do
+      mkdir -p $PKGDIR/$i/$j
+      cp $BASEDIR/testfile $PKGDIR/$i/testfile-$j
+
       for (( k = 1; k <= $inner; k++ )); do
+        mkdir -p $PKGDIR/$i/$j/$k
+        cp $BASEDIR/testfile $PKGDIR/$i/$j/testfile-$k
+        
         for (( a = 1; a <= $inner; a++ )); do
+          mkdir -p $PKGDIR/$i/$j/$k/$a
+          cp $BASEDIR/testfile $PKGDIR/$i/$j/$k/testfile-$a
+
           for (( b = 1; b <= $inner; b++ )); do
             mkdir -p $PKGDIR/$i/$j/$k/$a/$b
+            cp $BASEDIR/testfile $PKGDIR/$i/$j/$k/$a/testfile-$b
+
             for (( c = 1; c <= $inner; c++ )); do
               cp $BASEDIR/testfile $PKGDIR/$i/$j/$k/$a/$b/testfile-$c
             done
